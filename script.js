b31bcd8c25ea5a8407db7d6ea44548f308beff09
Victor Volkov <vityan2504@gmail.com>
2022-09-04 17:51:46 +0300

    start template

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth / 2;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
